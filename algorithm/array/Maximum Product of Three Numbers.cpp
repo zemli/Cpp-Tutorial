d503beaa5233237b25447c97e5ca83f013ed7bda
Zeming Li <zemingli777@gmail.com>
2017-09-04 08:52:47 +0800

    Update Maximum Product of Three Numbers.cpp

--- algorithm/array/Maximum Product of Three Numbers.cpp
+++ algorithm/array/Maximum Product of Three Numbers.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end());
         int i = nums.size()-1;
         int res1 = nums[i-2]*nums[i-1]*nums[i];
-        int res2 = nums[0]*nums[1]*nums[i];
+        int res2 = nums[0]*nums[1]*nums[i];//just in case of two large negative numbers
         int max = (res1>res2)?res1:res2;
         return max;
     }
